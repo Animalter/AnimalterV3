bb1dd8baa14133b94da251358eb5a61d09b1681b
muhammed balıkçı <muhammedbalikci76@gmail.com>
2023-12-22 19:00:24 +0300

    RoleController

---
+++ AnimalterV3/Controllers/RoleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalterV3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+    }
+}
